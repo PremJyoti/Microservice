3b22d292e4c8fd55bb82489cc68522fc83981a8a
PremJPC <premj644.pj@gmail.com>
2025-01-19 20:58:22 +0530

    Added global exception handler

---
+++ Hostel/src/main/java/com/mircro/Mirco1/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.mircro.Mirco1.Exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Handle specific ResourceNotFoundException
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(CustomException ex, WebRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("message", ex.getMessage());
+        response.put("details", request.getDescription(false));
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    // Handle global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGlobalException(Exception ex, WebRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("message", "An error occurred");
+        response.put("details", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
